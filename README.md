5a32f76a608cc050908b96516c589144e6d067bb
noahmmcgivern <noah.m.mcgivern@gmail.com>
2020-03-19 17:23:47 -0400

    Documentation++

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # duck-intertyper
-Uses normal distribution to randomly space out timing between keystrokes when using STRING and TAB in [DuckScript](https://github.com/hak5darren/USB-Rubber-Ducky/wiki/Duckyscript).
+Uses [normal distribution](https://en.wikipedia.org/wiki/Normal_distribution) from [numpy](https://numpy.org/) to randomly space out timing between keystrokes when using STRING and TAB in [Ducky Script](https://github.com/hak5darren/USB-Rubber-Ducky/wiki/Duckyscript).
+
+## Installation
+```
+python -m pip install -r requirements.txt
+```
+
+## Usage
+Write to STDOUT in the terminal:
+```
+python main.py example.txt
+```
+
+Write to a file:
+```
+python main.py example.txt > output.txt
+```
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-#!/usr/bin/env python3
 import numpy
 import sys
+from typing import List
 
-MU: int = 130
-SIGMA: int = 25
+MU: int = 130  # Mean
+SIGMA: int = 25  # Standard Deviation
+
 
 def delay() -> None:
+    """Write a delay with a random time to STDOUT."""
     print("DELAY " + str(int(numpy.random.normal(MU, SIGMA))))
 
-def get_lines(filename: str):
+
+def get_lines(filename: str) -> List[str]:
+    """Return a list of lines for the file at the given filename."""
     try:
         f = open(filename, "r")
         return f.read().splitlines()
@@ -17,22 +21,28 @@ def get_lines(filename: str):
     finally:
         f.close()
 
+
 def intertype(line: str) -> None:
-    for char in line[7:]:
+    """Writes a String to STDOUT, character-by-character, with a delay in-between."""
+    for char in line:
         if char == " ":
             print("SPACE")
         else:
             print("STRING " + char)
         delay()
 
+
 def main(filename: str) -> None:
+    """Takes in a Ducky Script file. Adds a random DELAY in-between characters. Writes output to STDOUT."""
     for line in get_lines(filename):
         if line[:6] == "STRING":
-            intertype(line)
+            intertype(line[7:])
         elif line[:3] == "TAB":
             print(line)
             delay()
         else:
             print(line)
 
-if __name__ == "__main__" and len(sys.argv) == 2: main(sys.argv[1])
+
+if __name__ == "__main__" and len(sys.argv) == 2:
+    main(sys.argv[1])
